b6384fd75b09abbe4f8d1728f5a802af2100569e
Frank Capillo <julio2capillo@gmail.com>
2023-07-14 21:17:27 -0500

    terminando agregar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,102 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from "vue"
+
 import empresas from './empresas.json' 
+
+const DEAFULT_FORM_DATA = {
+  id: null,
+  name: ' ',
+  owner: ' '
+}
+
+const enterprises = ref(empresas)
+
+const formData = ref(DEAFULT_FORM_DATA)
+
+const onSubmit = () => {
+
+  const {id, name, owner} = formData.value
+
+  const newId = crypto.randomUUID()
+  const newEnterprise = {
+    id: newId,
+    name,
+    owner
+  }
+  enterprises.value = [
+    ...enterprises.value,
+    newEnterprise
+  ]
+
+  console.log('guardando data...', newEnterprise);
+
+}
+ 
 </script>
 
 <template>
-<div class="container">
-  <h1>Primer challenge</h1>
-  <table>
-    <thead>
-      <tr>
-      <th>#</th>
-      <th>Name</th>
-      <th>Owner</th>
-    </tr>
-    </thead>
-    <tbody>
-      <tr v-for="empresa in empresas" :key="empresa.id">
-        <td>{{empresa.id}}</td>
-        <td>{{empresa.name}}</td>
-        <td>{{empresa.owner}}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <div class="container">
+    <nav>
+      <ul>
+        <li>Lista de empresas</li>
+      </ul>
+      <ul>
+        <li>
+          <button role="button">Nuevo company</button>
+        </li>
+      </ul>
+    </nav>
+    <div class="grid">
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Owner</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="empresa in enterprises" :key="empresa.id">
+              <td>{{ empresa.id }}</td>
+              <td>{{ empresa.name }}</td>
+              <td>{{ empresa.owner }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <!-- {{ formData }} -->
+        <form @submit.prevent="onSubmit">
+          <label>
+            Nombre de la empresa
+            <input 
+            type="text"
+            placeholder="Ex. Mi empresa"
+            required
+            v-model="formData.name"
+            />
+          </label>
+          <label>
+            Nombre propietario
+            <input 
+            type="text"
+            placeholder="Ex. Capillo"
+            required
+            v-model="formData.owner"
+            />
+          </label>
+          <button type="submit">
+            Guardar
+          </button>
+        </form>
+      </div>
+
+    </div>
+
+
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
